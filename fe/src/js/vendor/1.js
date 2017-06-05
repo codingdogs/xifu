15e9081937cfb63bd0a753d143f8ed35a7e7fa82
严浩韦 <704175885@qq.com>
2017-06-05 15:15:47 +0800

    U

--- fe/src/js/vendor/1.js
+++ fe/src/js/vendor/1.js
@@ -2,9 +2,5 @@
  * @Author: yanhaowei
  * @Date:   2017-05-28T18:05:04+08:00
  * @Last modified by:   yanhaowei
- * @Last modified time: 2017-05-28T18:05:46+08:00
+ * @Last modified time: 2017-06-05T14:09:00+08:00
  */
-
-
-
-alerT(44444)
--- fe/src/js/vendor/2.js
+++ fe/src/js/vendor/2.js
@@ -2,9 +2,5 @@
  * @Author: yanhaowei
  * @Date:   2017-05-29T08:30:12+08:00
  * @Last modified by:   yanhaowei
- * @Last modified time: 2017-05-29T08:30:17+08:00
+ * @Last modified time: 2017-06-05T14:08:58+08:00
  */
-
-
-
-alert(22222)
